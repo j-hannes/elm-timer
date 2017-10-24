3a7a9e59c4f3abea1cd9b48df2a6209f249154c4
j-hannes <hannes_e@gmx.de>
2017-10-24 09:40:06 +0200

    SVG animation of timer

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (Html, text, div, img, button, input)
 import Html.Attributes exposing (value, class, placeholder)
 import Html.Events exposing (onClick)
+import Svg exposing (circle, line, svg)
+import Svg.Attributes exposing (viewBox, width, cx, cy, r, fill, x1, y1, x2, y2, stroke)
 import Time exposing (Time, millisecond, inMilliseconds)
 import Task
 
@@ -104,12 +106,38 @@ viewButton model =
 
 viewTime : Maybe Timer -> Html Msg
 viewTime timer =
-    case timer of
-        Nothing ->
-            div [] []
+    div []
+        (case timer of
+            Nothing ->
+                []
 
-        Just { elapsed } ->
-            div [] [ text <| toString elapsed ]
+            Just { elapsed } ->
+                -- div [] [ text <| toString elapsed ]
+                let
+                    angle =
+                        Debug.log "angle" <|
+                            turns <|
+                                Debug.log "elapsed" <|
+                                    pi
+                                        * toFloat elapsed
+                                        / (25 * 60 * ticksPerSecond)
+
+                    handX =
+                        toString (50 + 50 * cos angle)
+
+                    handY =
+                        toString (50 + 50 * sin angle)
+                in
+                    [ viewSvg handX handY ]
+        )
+
+
+viewSvg : String -> String -> Html Msg
+viewSvg handX handY =
+    svg [ viewBox "0 0 100 100", width "300px" ]
+        [ circle [ cx "50", cy "50", r "50", fill "#0B79CE" ] []
+        , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+        ]
 
 
 
