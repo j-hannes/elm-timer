218f2c61c477a43e72637b6cd9c180d2a3843b9d
j-hannes <hannes_e@gmx.de>
2017-10-26 09:06:18 +0200

    WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,28 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (Html, text, div, img, button, input)
-import Html.Attributes exposing (value, class, placeholder)
+import Html.Attributes exposing (value, class, placeholder, attribute)
 import Html.Events exposing (onClick)
-import Svg exposing (circle, line, svg)
-import Svg.Attributes
-    exposing
-        ( viewBox
-        , width
-        , cx
-        , cy
-        , r
-        , fill
-        , x1
-        , y1
-        , x2
-        , y2
-        , stroke
-        , strokeDasharray
-        , strokeDashoffset
-        , strokeWidth
-        , transform
-        )
-import Time exposing (Time, millisecond, inMilliseconds)
+
+
+-- import Svg exposing (circle, line, svg)
+-- import Svg.Attributes exposing (..)
+
+import Time exposing (Time, millisecond, inMilliseconds, second)
 import Task
 
 
@@ -32,6 +18,11 @@ import Task
 ticksPerSecond : Float
 ticksPerSecond =
     30
+
+
+timerTimeInSeconds : Float
+timerTimeInSeconds =
+    10
 
 
 
@@ -44,13 +35,20 @@ type alias Timer =
     }
 
 
+type PieState
+    = None
+    | Half
+    | Full
+    | Finished
+
+
 type alias Model =
-    { timer : Maybe Timer }
+    { timer : Maybe Timer, pie : PieState }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { timer = Nothing }, Cmd.none )
+    ( { timer = Nothing, pie = None }, Cmd.none )
 
 
 
@@ -71,15 +69,22 @@ update msg model =
             ( model, Task.perform SetStartTime Time.now )
 
         StopTimer ->
-            ( { model | timer = Nothing }, Cmd.none )
+            ( { model | timer = Nothing, pie = None }, Cmd.none )
 
         SetStartTime time ->
-            ( { model | timer = Just <| Timer time 0 }, Cmd.none )
+            ( { model | timer = Just <| Timer time 0, pie = Half }, Cmd.none )
 
         Tick time ->
             case model.timer of
                 Just timer ->
-                    ( { model | timer = Just (updateTimer timer time) }, Cmd.none )
+                    let
+                        pie =
+                            if (model.pie == Half) then
+                                Full
+                            else
+                                None
+                    in
+                        ( { model | timer = Just (updateTimer timer time), pie = pie }, Cmd.none )
 
                 Nothing ->
                     ( model, Cmd.none )
@@ -107,7 +112,7 @@ view model =
         [ input [ class "time-input", value "25", placeholder "mm" ] []
         , input [ class "time-input", value "00", placeholder "ss" ] []
         , viewButton model
-        , div [ class "time-chart" ] [ viewTime model.timer ]
+        , div [ class "time-chart" ] [ viewTime model.timer model.pie ]
         ]
 
 
@@ -121,47 +126,65 @@ viewButton model =
             button [ onClick StopTimer ] [ text "Stop" ]
 
 
-viewTime : Maybe Timer -> Html Msg
-viewTime timer =
+viewTime : Maybe Timer -> PieState -> Html Msg
+viewTime timer pie =
     case timer of
         Nothing ->
-            viewPie 0
+            viewPie 0 None
 
         Just { elapsed } ->
-            viewPie ((toFloat elapsed) / (1 * 60 * ticksPerSecond))
+            let
+                percentage =
+                    (toFloat elapsed) / (1 * 60 * ticksPerSecond)
+            in
+                viewPie percentage pie
 
 
-viewPie : Float -> Html Msg
-viewPie percentage =
-    svg [ viewBox "0 0 100 100", width "300px" ]
-        [ circle
-            [ cx "50"
-            , cy "50"
-            , r "40"
+viewPie : Float -> PieState -> Html Msg
+viewPie percentage pie =
+    div
+        [ class "pie"
+        , class
+            (if pie == Half then
+                "one"
+             else
+                ""
+            )
 
-            -- , fill "#0B79CE"
-            , fill "white"
-            , stroke "#0B79CE"
-            ]
-            []
-        , circle
-            [ cx "50"
-            , cy "50"
-            , r "20"
-
-            -- , fill "#0B79CE"
-            -- , stroke "white"
-            , fill "white"
-            , stroke "#0B79CE"
-            , strokeWidth "40"
-            , strokeDasharray ((toString (percentage * 126)) ++ " 126")
-            , transform "rotate(-90 50 50)"
-            ]
-            []
+        -- , class
+        --     (if percentage > 0 then
+        --         "active"
+        --      else
+        --         ""
+        --     )
+        , attribute "data-start" "100"
+        , attribute "data-value" "260"
         ]
+        []
 
 
 
+-- svg [ viewBox "0 0 100 100", width "300px" ]
+--     [ circle
+--         [ cx "50"
+--         , cy "50"
+--         , r "40"
+--         , fill "white"
+--         , stroke "#0B79CE"
+--         ]
+--         []
+--     , circle
+--         [ cx "50"
+--         , cy "50"
+--         , r "20"
+--         , fill "white"
+--         , stroke "#0B79CE"
+--         , strokeWidth "40"
+--         , strokeDasharray ((toString (percentage * 126)) ++ " 126")
+--         , transform "rotate(-90 50 50)"
+--         ]
+--         []
+--     ]
 ---- SUBSCRIPTIONS ----
 
 
@@ -172,7 +195,7 @@ subscriptions model =
             Sub.none
 
         _ ->
-            Time.every (1000 * millisecond / ticksPerSecond) Tick
+            Time.every (timerTimeInSeconds * second / 2) Tick
 
 
 
